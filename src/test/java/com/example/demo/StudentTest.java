81423b8424745b522f11e94ec28e8e59f55b091b
崔定豪 <1014698129@qq.com>
2018-10-10 11:52:54 +0800

    Test配置

---
+++ src/test/java/com/example/demo/StudentTest.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Description:
+ *
+ * @author 任耀
+ * @ClassName: StudentTest
+ * @date 2018/10/10 11:08
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class StudentTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testMethod() {
+
+    }
+
+
+}
---
+++ src/test/java/com/example/demo/controller/SeatControllerTest.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Description:
+ *
+ * @author 任耀
+ * @ClassName: SeatControllerTest
+ * @date 2018/10/10 11:50
+ */
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SeatControllerTest {
+
+    @Test
+    public void getSeat() {
+
+    }
+
+    @Test
+    public void outSeat() {
+
+    }
+
+    @Test
+    public void tempOut() {
+
+    }
+}
